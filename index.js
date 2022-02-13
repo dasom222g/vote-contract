83f35b0e5bfde76e4655a38f32d169f803b0651a
dasom <dasom228@gmail.com>
2022-02-13 19:43:05 +0900

    Fix script type

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+import Web3 from './node_modules/web3'
 // var Web3 = require("web3")
 var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
 var account;
 
 var abi = JSON.parse('[{"inputs":[{"internalType":"bytes32[]","name":"candidateList","type":"bytes32[]"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"candidates","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"candidate","type":"bytes32"}],"name":"getCanditeNumberOfVotes","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"candidate","type":"bytes32"}],"name":"validCandidate","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"name":"voteRecived","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"candidate","type":"bytes32"}],"name":"votingForCandidate","outputs":[],"stateMutability":"nonpayable","type":"function"}]')
-var VotingContract = web3.eth.contract(abi)
+var VotingContract = web3.eth.contract(abi);
 
 var contractInstance = VotingContract.at('0xeeac5d8c9c6a77a36226a081b67d1ccf22c07276');
 
@@ -17,7 +18,7 @@ $(document).ready(function() {
       }
   });
 
-  function voteForCandidate(cd) {
+  function voteForCandidate() {
     candidateName = $("#candidate").val();
   
     contractInstance.votingForCandidate(candidateName, {from: account, gas: 4700000}, function() {
