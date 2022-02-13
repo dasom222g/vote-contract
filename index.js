26d0b3518f0500f0e5472436147de2e2da0f33c0
dasom <dasom228@gmail.com>
2022-02-13 19:43:22 +0900

    Fix script type

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import Web3 from './node_modules/web3'
+// import Web3 from './node_modules/web3'
 // var Web3 = require("web3")
 var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
 var account;
